a0a9f4c5959b409b931f6eeba90750d0b97f605e
Falcomfr <falcominien@yahoo.fr>
2022-11-10 16:20:11 +0100

    Add tpToMe command

---
+++ Commands/TeleportToMe.cs
@@ -0,0 +1,69 @@
+﻿using NetworkMessages.FromServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+namespace ChatCommands.Commands
+{
+    class TeleportToMe : Command
+    {
+        public bool CanUse(NetworkCommunicator networkPeer)
+        {
+            bool isAdmin = false;
+            bool isExists = AdminManager.Admins.TryGetValue(networkPeer.VirtualPlayer.Id.ToString(), out isAdmin);
+            return isExists && isAdmin;
+        }
+
+        public string Command()
+        {
+            return "!tptome";
+        }
+
+        public string Description()
+        {
+            return "Teleport player to you. Usage !tptome <Target User>";
+        }
+
+        public bool Execute(NetworkCommunicator networkPeer, string[] args)
+        {
+            if (args.Length == 0)
+            {
+                GameNetwork.BeginModuleEventAsServer(networkPeer);
+                GameNetwork.WriteMessage(new ServerMessage("Please provide a username."));
+                GameNetwork.EndModuleEventAsServer();
+                return true;
+            }
+
+            NetworkCommunicator targetPeer = null;
+            foreach (NetworkCommunicator peer in GameNetwork.NetworkPeers)
+            {
+                if (peer.UserName.Contains(string.Join(" ", args)))
+                {
+                    targetPeer = peer;
+                    break;
+                }
+            }
+            if (targetPeer == null)
+            {
+                GameNetwork.BeginModuleEventAsServer(networkPeer);
+                GameNetwork.WriteMessage(new ServerMessage("Target player not found"));
+                GameNetwork.EndModuleEventAsServer();
+                return true;
+            }
+
+
+            if (networkPeer.ControlledAgent != null && targetPeer.ControlledAgent != null) {
+                Vec3 targetPos = networkPeer.ControlledAgent.Position;
+                targetPos.x = targetPos.x + 1;
+                targetPeer.ControlledAgent.TeleportToPosition( targetPos );
+            }
+
+
+            return true;
+        }
+    }
+}
