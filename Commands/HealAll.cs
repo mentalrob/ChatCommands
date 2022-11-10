b705e2c8140ac70030cc7da4f4db55770a4a2119
Falcomfr <falcominien@yahoo.fr>
2022-11-10 15:08:56 +0100

    Add healing commands

---
+++ Commands/HealAll.cs
@@ -0,0 +1,40 @@
+﻿using NetworkMessages.FromServer;
+using TaleWorlds.MountAndBlade;
+
+namespace ChatCommands.Commands
+{
+    class HealAll : Command
+    {
+        public bool CanUse(NetworkCommunicator networkPeer)
+        {
+            bool isAdmin = false;
+            bool isExists = AdminManager.Admins.TryGetValue(networkPeer.VirtualPlayer.Id.ToString(), out isAdmin);
+            return isExists && isAdmin;
+        }
+
+        public string Command()
+        {
+            return "!healall";
+        }
+
+        public string Description()
+        {
+            return "Healing all players.";
+        }
+
+        public bool Execute(NetworkCommunicator networkPeer, string[] args)
+        {
+            foreach (NetworkCommunicator peer in GameNetwork.NetworkPeers)
+            {
+                if(peer.ControlledAgent != null)
+                {
+                    peer.ControlledAgent.Health = peer.ControlledAgent.HealthLimit;
+                    GameNetwork.BeginModuleEventAsServer(peer);
+                    GameNetwork.WriteMessage(new ServerMessage("Players are heal"));
+                    GameNetwork.EndModuleEventAsServer();
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Commands/HealMe.cs
@@ -0,0 +1,34 @@
+﻿using TaleWorlds.MountAndBlade;
+
+namespace ChatCommands.Commands
+{
+    class HealMe : Command
+    {
+        public bool CanUse(NetworkCommunicator networkPeer)
+        {
+            bool isAdmin = false;
+            bool isExists = AdminManager.Admins.TryGetValue(networkPeer.VirtualPlayer.Id.ToString(), out isAdmin);
+            return isExists && isAdmin;
+        }
+
+        public string Command()
+        {
+            return "!healme";
+        }
+
+        public string Description()
+        {
+            return "Healing yourself.";
+        }
+
+        public bool Execute(NetworkCommunicator networkPeer, string[] args)
+        {
+            if (networkPeer.ControlledAgent != null)
+            {
+                networkPeer.ControlledAgent.Health = networkPeer.ControlledAgent.HealthLimit;
+
+            }
+            return true;
+        }
+    }
+}
